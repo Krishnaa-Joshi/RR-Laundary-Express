49a464d12b1837d9ce69fdbbea185c8043570a39
Krishna Joshi <krishnajos01@gmail.com>
2026-02-11 16:35:01 +0530

    nav-link js code fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const currPage = window.location.pathname.split("/").pop();
+  const navLinks = document.querySelectorAll(".nav-link");
 
-  const navPage = document.querySelectorAll(".nav-link");
+  navLinks.forEach(link => {
+    const page = link.getAttribute("href");
 
-  navPage.forEach(link => {
-    const Page = link.getAttribute("href");
+    if (link.classList.contains("dropdown-toggle")) return;
 
-    if (Page === currPage || 
-        (currPage === "" && Page === "index.html")) {
+    if (
+      page === currPage ||
+      (currPage === "" && page === "index.html")
+    ) {
       link.classList.add("active");
     }
   });
 });
+
